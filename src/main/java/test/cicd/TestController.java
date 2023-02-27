bf89887939ef4c185b313e19900d1c36e598a0c0
jhl2324 <100778162+jhl2324@users.noreply.github.com>
2023-02-28 07:19:54 +0900

    Update TestController.java

--- src/main/java/test/cicd/TestController.java
+++ src/main/java/test/cicd/TestController.java
@@ -7,12 +7,22 @@ import org.springframework.web.bind.annotation.RestController;
 public class TestController {
 
     @GetMapping("/test1")
-    public String test(){
+    public String test1(){
         return "TEST1";
     }
     
     @GetMapping("/test2")
     public String test2(){
         return "TEST2";
+    }
+    
+    @GetMapping("/test3")
+    public String test3(){
+        return "TEST3";
+    }
+    
+    @GetMapping("/test4")
+    public String test4(){
+        return "TEST4";
     }
 }
