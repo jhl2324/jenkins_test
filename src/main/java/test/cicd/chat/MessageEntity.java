b7512fcb887d1c6a6f61cd220d071435448301ea
jhl2324 <dlwogus8888@ajou.ac.kr>
2023-03-09 09:01:43 +0900

    test

--- src/main/java/test/cicd/chat/MessageEntity.java
+++ src/main/java/test/cicd/chat/MessageEntity.java
@@ -4,20 +4,15 @@ import lombok.Getter;
 import lombok.NoArgsConstructor;
 import lombok.Setter;
 
-import javax.persistence.Entity;
-import javax.persistence.GeneratedValue;
-import javax.persistence.GenerationType;
-import javax.persistence.Id;
-
-@Entity
+//@Entity
 @Getter
 @Setter
 @NoArgsConstructor
 public class MessageEntity {
 
-    @Id
-    @GeneratedValue(strategy = GenerationType.AUTO)
-    private Long id;
+//    @Id
+//    @GeneratedValue(strategy = GenerationType.AUTO)
+//    private Long id;
 
     private String text;
 
--- src/main/java/test/cicd/chat/MessageReceiver.java
+++ src/main/java/test/cicd/chat/MessageReceiver.java
@@ -4,8 +4,6 @@ import lombok.extern.slf4j.Slf4j;
 import org.springframework.jms.annotation.JmsListener;
 import org.springframework.stereotype.Component;
 
-import javax.persistence.EntityManager;
-import javax.transaction.Transactional;
 import java.net.InetAddress;
 import java.net.UnknownHostException;
 import java.time.LocalDateTime;
@@ -14,19 +12,19 @@ import java.time.LocalDateTime;
 @Slf4j
 public class MessageReceiver {
 
-    private final EntityManager entityManager;
+    //private final EntityManager entityManager;
 
-    public MessageReceiver(EntityManager entityManager) {
-        this.entityManager = entityManager;
-    }
+//    public MessageReceiver(EntityManager entityManager) {
+//        this.entityManager = entityManager;
+//    }
 
-    @Transactional
+    //@Transactional
     @JmsListener(destination = "message.queue")
     public void receiveMessage(String message) throws UnknownHostException {
         try {
             log.info("[{}] thread : {}, time : {}, message : {} - receiver - start", InetAddress.getLocalHost().getHostAddress(), Thread.currentThread().getId(), message, LocalDateTime.now());
             MessageEntity entity = new MessageEntity(message);
-            entityManager.persist(entity);
+      //      entityManager.persist(entity);
         } catch (Exception e) {
             throw new RuntimeException(e);
         } finally {
